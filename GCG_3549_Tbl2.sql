2a3dc90b000bc698da2ac05f97e90ef1a974ce65
Service Web <sw@gssmail.com>
2024-03-08 13:06:28 -0600

    Commit changes in C:\Websites\Git\emarlow\3549\GCG_3549_Tbl2.sql

---
+++ GCG_3549_Tbl2.sql
@@ -0,0 +1,24 @@
+/* ==========================================
+ * TABLE: GCG_3549_Tbl2
+ * ========================================== */
+CREATE TABLE "GCG_3549_Tbl2"(
+ "JobPart" VARCHAR(50),
+ "workcenter" VARCHAR(50),
+ "seq" VARCHAR(50),
+ "LMO" VARCHAR(50),
+ "product_line" VARCHAR(50),
+ "qty_order" DOUBLE,
+ "OPPart" VARCHAR(50),
+ "job" VARCHAR(50),
+ "Units_Scrap" DOUBLE,
+ "Hours_Estimated" DOUBLE,
+ "Hours_Actual" DOUBLE,
+ "Dollars_Estimated" DOUBLE,
+ "Dollars_Actual" DOUBLE,
+ "OpStdate" DATETIME,
+ "HdrStdate" DATETIME,
+ "router" VARCHAR(50),
+ "router_seq" VARCHAR(50),
+ "flag_setup" VARCHAR(10));
+
+
