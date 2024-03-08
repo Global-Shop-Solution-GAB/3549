8ad7d22fb2e15f19e1fc60d4b7975c32b09a1c56
Service Web <sw@gssmail.com>
2024-03-08 13:06:28 -0600

    Commit changes in C:\Websites\Git\emarlow\3549\GCG_3549_Job_EstAct.sql

---
+++ GCG_3549_Job_EstAct.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "GCG_3549_Job_EstAct"(
+ "Level" VARCHAR(20),
+ "Component" VARCHAR(80),
+ "WCenter" VARCHAR(100),
+ "EstHour" DOUBLE,
+ "ActHour" DOUBLE,
+ "EstLabor" DOUBLE,
+ "ActLabor" DOUBLE,
+ "MatRecCost" DOUBLE,
+ "MatWEIAverage" DOUBLE,
+ "ScrapGB" VARCHAR(50),
+ "ID" IDENTITY);
